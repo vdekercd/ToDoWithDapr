12b4966097df46d7d77e52387b541dea1b35d739
DKE <damien.vandekerckhoven@aweurope.be>
2021-10-03 12:24:42 +0200

    In progress commit

---
+++ ToDoWithDapr.API/Controllers/V1/TodoController.cs
@@ -0,0 +1,40 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using ToDoWithDapr.API.Models;
+
+namespace ToDoWithDapr.API.Controllers.V1
+{
+    [ApiController]
+    [Route("V1/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly DaprClient _daprClient;
+
+        private const string STORE_NAME = "statestore";
+        private const string KEY = "todos";
+
+        public TodoController(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetAsync()
+        {
+            return Ok(await GetAllTodoAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Todo todo)
+        {
+            var todos = await GetAllTodoAsync();
+            if (todos == null) todos = new List<Todo>();
+            todos.Add(todo);
+            await SaveAllTodosAsync(todos);
+            return Ok();
+        }
+
+        private async Task<List<Todo>> GetAllTodoAsync() => await _daprClient.GetStateAsync<List<Todo>>(STORE_NAME, KEY);
+        private async Task SaveAllTodosAsync(List<Todo> todos) => await _daprClient.SaveStateAsync<List<Todo>>(STORE_NAME, KEY, todos);
+    }
+}
---
+++ ToDoWithDapr.API/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoWithDapr.API.Models
+{
+    public class Todo
+    {
+        public int Id {  get; set; }
+        public string Description {  get; set; }
+        public DateTime CreatedDate {  get; set; }
+        public bool IsDone { get; set; }
+    }
+}
